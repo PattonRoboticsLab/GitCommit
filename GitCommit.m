689680d64b258179a72c889cff8ba4f38cbf7e55
loshea1 <86704069+loshea1@users.noreply.github.com>
2024-06-06 12:16:27 -0500

    Update GitCommit.m

--- GitCommit.m
+++ GitCommit.m
@@ -1,11 +1,6 @@
 % This script commits and pushes all changes to the desired branch in a GitHub repository.
 
-function GitCommit(message, Path1, Path2)
-cd(Path2)
-!git pull
-disp('Test test test')
-
-cd(Path1)
+function GitCommit(message)
 
 % CHECKS
 [status1, cmdout1] = system('git --version');         % Check if Git is installed
